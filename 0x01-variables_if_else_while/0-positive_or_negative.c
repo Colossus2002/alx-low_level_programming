7511cb167d6cf40bc116dc26aad7bfaedfcf41ec
Colossus2002 <nedukeyz55@gmail.com>
2022-09-21 03:42:35 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,25 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *  * main - entry point of programs
- *   *
- *    * Return: return 0
- *     */
+ * main - entry point of progress
+ *
+ * Return: return 0
+ */
 int main(void)
-{
-		int n;
+  {
+  	  int n;
+	  
+	  srand(time(0);
+	  n = rand() - RAND_MAX / 2;
+          if (n < 0)
+	  {
+	         printf("%d is negative\n", n);
+	  
+	  }
+ 	  else if (n > 0)
+	  
+	  {
+	        printf("%d is positive\n", n):
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-					if (n < 0)
-							{
-										printf("%d is negative\n", n);
-											}
-						else if (n > 0)
-								{
-											printf("%d is positive\n", n);
-												}
-							else
-										printf("%d is zero\n", n);
-								return (0);
-}
+	  }
+          else
+                printf("%d is zero\n", n);
+          
+          return (0);
+
+  }	  
