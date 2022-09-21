f4d6b8e452f06898007bd5e290c1f8027f991529
Colossus2002 <nedukeyz55@gmail.com>
2022-09-21 06:26:05 -0700

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,21 @@
 #include "main.h"
 /**
- * print_alphabet - Prints alphabets 'a - z'
+ * print_alphabet_x10 - Prints the alphabet 10 times
+ *
+ * Return: void
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-	char c;
+	int i;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(c);
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
